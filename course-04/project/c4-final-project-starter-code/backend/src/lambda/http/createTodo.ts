33b7f331f8e5b360eb817a738eac7259a7f5e5e2
rochana <rochana.jayakumar@gmail.com>
2021-07-09 21:21:53 -0400

    Remove commented out code

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -5,34 +5,15 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils'
 import { createLogger } from '../../utils/logger'
 import { createTodos } from '../../datahelpers/todolayers'
-//import * as AWS  from 'aws-sdk'
-// import * as uuid from 'uuid'
-
-// const docClient = new AWS.DynamoDB.DocumentClient()
-// const todosTable = process.env.TODOS_TABLE
 const logger = createLogger('getTodos')
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Enter createTodo')
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-  //const uniqueID = uuid.v4()
 
   const parsedUserId = getUserId(event)
   const newItem = await createTodos(parsedUserId, newTodo)
-
-  // logger.info("parsedUserId = ", parsedUserId);
-
-  // const newItem = {
-  //   userId: parsedUserId,
-  //   todoId: uniqueID,
-  //   ...newTodo
-  // }
-
-  // await docClient.put({
-  //   TableName: todosTable, 
-  //   Item: newItem
-  // }).promise()
 
   // TODO: Implement creating a new TODO item
   return {
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,43 +1,16 @@
 import 'source-map-support/register'
-//import * as AWS  from 'aws-sdk'
 import { createLogger } from '../../utils/logger'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import { getUserId } from '../utils'
 import { deleteTodos } from '../../datahelpers/todolayers'
 
 const logger = createLogger('deleteTodos')
-// const docClient = new AWS.DynamoDB.DocumentClient()
-// const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Enter deleteTodo')
   const todoId = event.pathParameters.todoId; 
   const userId = getUserId(event)
   await deleteTodos(todoId, userId)
-
-  // logger.info(`todoId = ${todoId} parsedUserId = ${userId}`)
-  // const newItem = await docClient.query({
-  //   TableName: todosTable,
-  //   KeyConditionExpression: 'userId = :userId',
-  //   ExpressionAttributeValues: {
-  //     ':userId': userId
-  //   }
-  // }).promise()
-
-  // const key = {
-  //   userId: userId, 
-  //   createdAt: newItem.Items[0].createdAt
-  // }
-
-  // logger.info(`Key = ${key}`)
-
-
-  // await docClient.delete({
-  //   TableName: todosTable,
-  //   Key: {
-  //     todoId, userId
-  //   }
-  // }).promise()
 
   return {
     statusCode: 204,
